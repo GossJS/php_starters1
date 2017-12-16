bb60a7c2ffd63db2195eeb28e07fb413b79c60bc
Ilya Goss <igossoudarev@herzen.spb.ru>
2017-12-16 17:22:20 +0300

    Create script.c

---
+++ script.c
@@ -0,0 +1,23 @@
+#include <time.h>   // Директива нужна для инициализации функции rand()
+#include <stdio.h>  // Включаем поддержку функций ввода/вывода
+#include <stdlib.h> // А это — для поддержки функции rand()
+
+// Главная функция. Именно она и запускается при старте сценария.
+int main(void) {
+  // инициализируем генератор случайных чисел
+  int num;
+  time_t t;
+  srand(time(&t));
+  // в num записывается случайное число от 0 до 9
+  num = rand() % 10;
+  printf("<!DOCTYPE html>");
+  printf("<html>");
+  printf("<head>");
+  printf("<title>Elias Goss CGI</title><meta charset='utf-8'>");
+  printf("</head>");
+  printf("<body>");
+  printf("<h1>Вас приветствует Elias Goss!</h1>");
+  printf("<p>Случайное число в диапазоне 0-9: %d</p>", num);
+  printf("</body>");
+  printf("</html>");
+}
